84d906a97e442252d38c19d68703f5e66359b3f4
Akshay Gupta <akshay.gupta@arm.com>
2018-02-21 07:53:44 +0000

    Uploading Code

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Board from './Board';
+import './index.css';
+
+ReactDOM.render(
+  <Board count={1} />,
+  document.getElementById('root')
+);
